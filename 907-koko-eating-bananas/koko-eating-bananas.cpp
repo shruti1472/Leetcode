5290e2eed12b9f6dcb2ba0487fe5c4574fa79871
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-10-07 20:56:18 +0530

    Time: 7 ms (72.26%) | Memory: 22.9 MB (74.38%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,23 +1,27 @@
 class Solution {
 
-public:
-long long count_hour(vector<int>& piles,int k){
-            long long total_hour=0;
-            for(int i=0;i<piles.size();i++){
-                total_hour+=ceil(piles[i]/double(k));
-            }
-          return total_hour;
-    }    
+// public:
+// long long count_hour(vector<int>& piles,int k){
+//             long long total_hour=0;
+//             for(int i=0;i<piles.size();i++){
+//                 total_hour+=(piles[i]+k-1/double(k));
+//             }
+//           return total_hour;
+//     }    
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
 
-    int maxval= *max_element(piles.begin(),piles.end());
-        int ans=maxval;
+    int high= *max_element(piles.begin(),piles.end());
+        int ans=high;
        int low=1;
-       int high=maxval;
+       
        while(low<=high){
+        long long total_hour=0;
         int mid=low+(high-low)/2;
-        if(count_hour(piles,mid)<=h){
+         for(int i=0;i<piles.size();i++){
+                total_hour+=(piles[i]+mid-1)/mid;
+            }
+        if(total_hour<=h){
             ans=mid;
             high=mid-1;
         }
