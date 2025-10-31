508c89a2b7f66a31eb9590c4ae8bbd50de0a6ab6
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-10-31 21:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (47.77%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,30 +1,32 @@
 class Solution {
-
-
-
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
-        int n=n1+n2;
-        int count=0;
-        int i=0,j=0;
-        int ele1=-1,ele2=-1;
-        for(;count<=n/2;count++){
-            ele1=ele2;
-            if(i<n1&&(j>=n2 || nums1[i]<nums2[j])){
-                ele2=nums1[i++];
-            }
-            else{
-                ele2=nums2[j++];
-            }
+       int n1=nums1.size();
+       int n2=nums2.size();
+       int n=n1+n2;
+       if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+       int low=0;
+       int high=n1;
+       int needed=(n1+n2+1)/2;
+       while(low<=high){
+        int mid1=low+(high-low)/2;
+        int mid2=needed-mid1;
+        int l1=INT_MIN;
+        int l2=INT_MIN;
+        int r1=INT_MAX;
+        int r2=INT_MAX;
+        if(mid1<n1) r1=nums1[mid1];
+        if(mid2<n2) r2=nums2[mid2];
+        if(mid1-1>=0) l1=nums1[mid1-1];
+        if(mid2-1>=0) l2=nums2[mid2-1];
+        if(l1<=r2 && l2<=r1){
+            if(n%2==1) return max(l1,l2);
+            return (double)(max(l1,l2)+min(r1,r2))/2.0;
         }
-
-        if(n%2==0){
-            return (ele1+ele2)/2.0;
-        }
-        else{
-            return ele2;
-        }
+        else if(l1 > r2) high=mid1-1;
+        else low=mid1+1;
+ 
+       }
+    return 0;
     }
 };
