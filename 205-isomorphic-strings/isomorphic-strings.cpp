adaf15cbb5226f46b80f77acff2ee77b8b3e4310
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-11-12 21:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (54.46%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,30 +3,16 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.size()!=t.size()) return false;
 
-        unordered_map<char,char>map_s;
-        unordered_map<char,char>map_t;
+    vector<int> mapS(256,-1);
+    vector<int> mapT(256,-1);
 
         for(int i=0;i<s.size();i++){
-            char c1=s[i];
-            char c2 =t[i];
+            if(mapS[s[i]]!=mapT[t[i]]) 
+            return false;
 
-            if(map_s.find(c1)!=map_s.end()){
-                if(map_s[c1]!= c2) return false;
-            }
-            else{
-                    map_s[c1]=c2;
-                }
-            
-
-            
-            if(map_t.find(c2)!=map_t.end()){
-                if(map_t[c2]!= c1) return false;
-            }
-            else{
-                    map_t[c2]=c1;
-                }
-            
-        }
+            mapS[s[i]]=i;
+            mapT[t[i]]=i;
+         }
         return true;
     }
 
