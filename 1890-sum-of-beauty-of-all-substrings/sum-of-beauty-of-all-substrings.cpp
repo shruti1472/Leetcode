35d2fcb6909459eab80ddff19a8cb5a6f0494480
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-11-17 20:57:12 +0530

    Time: 54 ms (81.73%) | Memory: 11.1 MB (67.33%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -5,10 +5,9 @@ public:
        
        int ans=0;
         for(int i=0;i<n;i++){
+            vector<int> freq(26, 0);
             for(int j=i;j<n;j++){
-               string sub=s.substr(i,j-i+1);
-                    vector<int> freq(26, 0);
-                    for(char c : sub) freq[c - 'a']++;
+                     freq[s[j] - 'a']++;
                     int mx = 0, mn = INT_MAX;
                     for(int f : freq) {
                     if(f == 0) continue; 
