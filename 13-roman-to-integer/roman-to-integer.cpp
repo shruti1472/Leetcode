e413b2a41cf10a52ade0e988c47326806d21f4b6
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-11-17 18:58:48 +0530

    Time: 2 ms (78.12%) | Memory: 10.1 MB (85.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n=s.size();
-        unordered_map<char,int>map;
+        static unordered_map<char,int>map;
         map['I'] = 1;
         map['V'] = 5;
         map['X'] = 10;
@@ -12,14 +12,14 @@ public:
         map['M'] = 1000;
 
          int result=0;
-         for(int i=n-1;i>=0;i--){
-             if( i+1<n && map[s[i]]<map[s[i+1]]){
+         for(int i=0;i<n-1;++i){
+             if( map[s[i]]<map[s[i+1]]){
                   result-=map[s[i]];
              }
              else{
                  result += map[s[i]];
              }
          }
-       return result;
+       return result+map[s.back()];
     }
 };
