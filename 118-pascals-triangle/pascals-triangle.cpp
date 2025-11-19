75b3352c238caaff86215b727406ab492b6cf546
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-11-19 20:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (32.08%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+
+    vector<int> getRow(int rowIndex) {
+        vector<int>ans(rowIndex+1);
+        long long value=1;
+        ans[0]=1;
+        for(int j=1;j<=rowIndex;j++){
+           value=value*(rowIndex-j+1)/j;
+           ans[j]=value;
+        }
+        return ans;
+    }
+
     vector<vector<int>> generate(int numRows) {
        vector<vector<int>>ans(numRows);
        for(int i=0;i<numRows;i++){
-        ans[i].resize(i+1);
-        ans[i][0]=ans[i][i]=1;
-            for(int j=1;j<i;j++){
-                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
-            }
+            ans[i]=getRow(i);
         }
         return ans;
-       
-       
+  
     }
 };
