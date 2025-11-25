1e905308fa383b7dd819176dd40a49eeb5dba7e0
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2025-11-25 21:45:53 +0530

    Time: 12 ms (44.34%) | Memory: 30.3 MB (60.40%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-     
-        int sum = 0;
-        vector<int> r1, r2;
-        
-        for (int x : nums) {
-            sum += x;
-            if (x % 3 == 1) r1.push_back(x);
-            else if (x % 3 == 2) r2.push_back(x);
+        vector<int>r1;
+        vector<int>r2;
+        int sum=0;
+        for(int i :nums){
+            sum+=i;
+            if(i%3==1) r1.push_back(i);
+            else if(i%3==2) r2.push_back(i);
         }
         
-        sort(r1.begin(), r1.end());
-        sort(r2.begin(), r2.end());
+        sort(r1.begin(),r1.end());
+        sort(r2.begin(),r2.end());
         
-        if (sum % 3 == 0) return sum;
-        
-        int remove = INT_MAX;
-        if (sum % 3 == 1) {
-            if (!r1.empty()) remove = min(remove, r1[0]);
-            if (r2.size() >= 2) remove = min(remove, r2[0] + r2[1]);
-        } else { // sum % 3 == 2
-            if (!r2.empty()) remove = min(remove, r2[0]);
-            if (r1.size() >= 2) remove = min(remove, r1[0] + r1[1]);
+        int remove=INT_MAX;
+        if(sum%3==0) return sum;
+       
+        if(sum%3==1){
+            if(!r1.empty()) remove=min(remove,r1[0]);
+            if(r2.size()>=2) remove=min(remove,r2[0]+r2[1]);
         }
-        
-        return sum - remove;
+        else {
+            if(!r2.empty()) remove=min(remove,r2[0]);
+            if(r1.size()>=2) remove=min(remove,r1[0]+r1[1]);
+        }
+        return sum-remove;
     }
 };
   
