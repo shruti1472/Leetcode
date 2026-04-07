e01f0bfa43cac7b4e1b59dd5dccca2fb72c36cb8
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-07 12:42:01 +0530

    Time: 8 ms (22.64%) | Memory: 118.1 MB (64.85%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -18,32 +18,56 @@ public:
 
 
        
-    ListNode* dummy = new ListNode(-1);
-    ListNode* tail = dummy;
-    ListNode* temp=head;
+    // ListNode* dummy = new ListNode(-1);
+    // ListNode* tail = dummy;
+    // ListNode* temp=head;
 
-    while (temp != nullptr) {
-        tail->next = new ListNode(temp->val);
-        tail = tail->next;
-        temp = temp->next;
-    }
+    // while (temp != nullptr) {
+    //     tail->next = new ListNode(temp->val);
+    //     tail = tail->next;
+    //     temp = temp->next;
+    // }
       
-    ListNode* newHead=dummy->next;
+    // ListNode* newHead=dummy->next;
 
-       ListNode* prev=nullptr;
-       ListNode* curr=head;
-       while(head!=nullptr){
-        head=head->next;
+    //    ListNode* prev=nullptr;
+    //    ListNode* curr=head;
+    //    while(head!=nullptr){
+    //     head=head->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=head;
+    //    }
+        
+    //    while(prev!=nullptr && newHead!=nullptr){
+    //     if(prev->val!=newHead->val) return false;
+    //     prev=prev->next;
+    //     newHead=newHead->next;
+    //    }
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+    while(fast->next && fast->next->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+
+    ListNode* prev=nullptr;
+    ListNode* curr=slow->next;
+    slow=slow->next;
+    while(slow){
+        slow=slow->next;
         curr->next=prev;
         prev=curr;
-        curr=head;
-       }
-        
-       while(prev!=nullptr && newHead!=nullptr){
-        if(prev->val!=newHead->val) return false;
+        curr=slow;
+    }
+
+    while(prev){
+        if(head->val!=prev->val) return false;
+        head=head->next;
         prev=prev->next;
-        newHead=newHead->next;
-       }
+    }
+
        return true;
       
 
