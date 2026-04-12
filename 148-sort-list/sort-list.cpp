4a13e3a75bf4bb47ed2d47a6f9f43fb7ee603697
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-12 12:36:58 +0530

    Time: 59 ms (8.59%) | Memory: 75.6 MB (37.84%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,26 +10,48 @@
  */
 class Solution {
 public:
-    
-
-    ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        int i=0;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-           arr.push_back(temp->val);
-            temp=temp->next;
-            i++;
+   ListNode* merge(ListNode* l1,ListNode* l2){
+    ListNode* temp1=l1;
+    ListNode* temp2=l2;
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp=dummy;
+    while(temp1 && temp2){
+        if(temp1->val < temp2->val){
+            temp->next=temp1;
+            temp1=temp1->next;
         }
-        sort(arr.begin(),arr.end());
-       int j=0;
-        temp=head;
-        while(temp!=nullptr){
-            temp->val=arr[j];
-            j++;
-            temp=temp->next;
+        else{
+            temp->next=temp2;
+            temp2=temp2->next;
         }
-        return head;
+        temp=temp->next;
     }
+    while(temp1){
+        temp->next=temp1;
+        temp=temp->next;
+        temp1=temp1->next;
+    }
+    while(temp2){
+        temp->next=temp2;
+        temp=temp->next;
+        temp2=temp2->next;
+    }
+    return dummy->next;
+}
 
+ListNode* sortList(ListNode* head){
+    if(head==nullptr || head->next==nullptr) return head;
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+    while(fast!=nullptr && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    ListNode* mid = slow->next;
+    slow->next = nullptr;
+    ListNode* left=sortList(head);
+    ListNode* right=sortList(mid);
+    ListNode* sorted=merge(left,right);
+    return sorted;
+}
 };
