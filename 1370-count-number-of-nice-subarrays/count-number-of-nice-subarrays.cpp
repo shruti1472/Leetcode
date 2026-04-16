07bc02bd5f8e988cd1c491ee949b37ace78d66bf
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-16 12:33:52 +0530

    Time: 4 ms (84.01%) | Memory: 77.8 MB (25.78%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
 
     int countlessthanequal(vector<int>nums,int k){
+        if (k < 0) return 0;
         int l=0,r=0;
         int sum=0;
         int count=0;
         while(r<nums.size()){
-            sum+=nums[r];
+            sum+=nums[r]%2;
             while(sum>k){
-                sum-=nums[l];
+                sum-=nums[l]%2;
                 l++;
             }
             count+=r-l+1;
@@ -18,14 +19,6 @@ public:
     }
 
     int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1;
-            }
-        }
         return countlessthanequal(nums,k)-countlessthanequal(nums,k-1);
     }
 };
