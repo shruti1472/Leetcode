5411cb20dfd8cf3fb0128cbbb1d1f5631208206c
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-26 14:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (38.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,20 +3,17 @@ public:
     int trap(vector<int>& h) {
         int sum=0;
         int n=h.size();
-        vector<int>prefix(n),suffix(n);
+        vector<int>prefix(n);
 
         prefix[0]=h[0];
         for(int i=1;i<n;i++){
             prefix[i]=max(prefix[i-1],h[i]);
         }
 
-        suffix[n-1]=h[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],h[i]);
-        }
-
-        for(int i=0;i<n;i++){
-            sum+=min(prefix[i],suffix[i])-h[i];
+        int rightmax=0;
+        for(int i=n-1;i>=0;i--){
+           rightmax=max(rightmax,h[i]);
+           sum+=min(prefix[i],rightmax)-h[i];
         }
         return sum;
     }
