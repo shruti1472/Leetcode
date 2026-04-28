d54e7d80cd715dbdfac949c1b4c47a667ae0a2a2
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-28 19:46:10 +0530

    Time: 24 ms (14.40%) | Memory: 51.1 MB (5.11%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -6,7 +6,7 @@ public:
         stack<pair<int,int>>st;
         vector<int>ans(n,n);
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top().first>=arr[i]){
+            while(!st.empty() && st.top().first>arr[i]){
                 st.pop();
             }
             if(!st.empty()) ans[i]=st.top().second;
@@ -20,7 +20,7 @@ public:
         stack<pair<int,int>>st;
         vector<int>ans(n,-1);
         for(int i=0;i<n;i++){
-            while(!st.empty() && st.top().first>arr[i]){
+            while(!st.empty() && st.top().first>=arr[i]){
                 st.pop();
             }
             if(!st.empty()) ans[i]=st.top().second;
