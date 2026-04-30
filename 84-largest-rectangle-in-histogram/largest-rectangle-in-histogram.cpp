9442b963fab117a0783673232a9107878dd2f562
shruti1472 <152212826+shruti1472@users.noreply.github.com>
2026-04-30 19:35:35 +0530

    Time: 24 ms (47.80%) | Memory: 81.4 MB (58.06%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
 
-    vector<int> nextSmallerElement(vector<int> &heights){
-        stack<pair<int,int>>st;
-        int n=heights.size();
-        vector<int>ans(n,n);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top().first>=heights[i]){
-                st.pop();
-            }
-            if(!st.empty()) ans[i]=st.top().second;
-            st.push({heights[i],i});
-        }
-        return ans;
-    }
-
-     vector<int> previousSmallerElement(vector<int> &heights){
-        stack<pair<int,int>>st;
-        int n=heights.size();
-        vector<int>ans(n,-1);
-        for(int i=0;i<n;i++){
-            while(!st.empty() && st.top().first>=heights[i]){
-                st.pop();
-            }
-            if(!st.empty()) ans[i]=st.top().second;
-            st.push({heights[i],i});
-        }
-        return ans;
-    }
-
+    
     int largestRectangleArea(vector<int>& heights) {
-        vector<int>PSE=previousSmallerElement(heights);
-        vector<int>NSE=nextSmallerElement(heights);
-        int maxi=0;
-        for(int i=0;i<heights.size();i++){
-            int current=(NSE[i]-PSE[i]-1)*heights[i];
-            maxi=max(current,maxi);
-        }
-        return maxi;
+       stack<int>st;
+       int n=heights.size();
+       int maxi=0;
+       for(int i=0;i<=n;i++){
+        int current= (i==n) ? 0 : heights[i];
+            while(!st.empty() && heights[st.top()]>current){
+                int h=heights[st.top()];
+                st.pop();
+
+                int right=i;
+                int left= st.empty() ?  -1 : st.top();
+                int width=right-left-1;
+                int area=h*width;
+                maxi=max(maxi,area);
+            }
+            st.push(i);
+       }
+       return maxi;
     }
 };
